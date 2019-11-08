4e13449bcabc34807cc97fe8d2187ed7649a1b0c
Allan Wambold <wambolda@uregina.ca>
2019-11-08 15:18:12 -0600

    done npm install in frontend created GroupPage component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupPageComponent } from './components/group-page/group-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupPageComponent
   ],
   imports: [
     BrowserModule,
