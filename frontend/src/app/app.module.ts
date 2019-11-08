9cbdb77ef59198f16841bf217826c78ad2ff9e8f
Allan Wambold <wambolda@uregina.ca>
2019-11-08 16:00:13 -0600

    Created message component Created members components installed bootstrap in frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GroupPageComponent } from './components/group-page/group-page.component';
+import { MessageComponent } from './components/message/message.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GroupPageComponent
+    GroupPageComponent,
+    MessageComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
