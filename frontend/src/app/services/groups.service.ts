37150527f665a28a27887c849b607e22474db009
Allan Wambold <wambolda@uregina.ca>
2019-11-12 15:14:55 -0600

    Gave each get function in groups.service.ts its own URL variable.

--- frontend/src/app/services/groups.service.ts
+++ frontend/src/app/services/groups.service.ts
@@ -11,19 +11,23 @@ import { catchError, tap } from 'rxjs/operators';
 })
 export class GroupsService {
 //TODO: Update URLs for get/post Requests
-  private _URL:string = "/assets/deleteThis.json";
   constructor(private http:HttpClient) { }
 
+  //URL for get messages
+  private _URLMessages:string = "/assets/deleteThis.json";
+
   getMessages(id: number):Observable<Message[]> {
-    return this.http.get<Message[]>(this._URL)
+    return this.http.get<Message[]>(this._URLMessages)
            .pipe(tap(result => result),
              catchError(this.errorHandler));
   }
 
-
+  //URL for create messages
+  private _URLCreateMessage:string = "/assets/deleteThis.json";
+ 
   createMessage(data:Message):Observable<object>{
     console.log(data);
-    return this.http.post(this._URL, data)
+    return this.http.post(this._URLCreateMessage, data)
     .pipe(tap(result => result));
   }
 
@@ -31,16 +35,19 @@ export class GroupsService {
     return throwError(error.message || "Server Error");
   }
 
-  private _URL2:string = "/assets/deleteThis2.json";
-
+  //URL for get tags
+  private _URLTag:string = "/assets/deleteThis2.json";
+  
   getTags():Observable<Tag[]> {
-    return this.http.get<Tag[]>(this._URL2)
+    return this.http.get<Tag[]>(this._URLTag)
            .pipe(catchError(this.errorHandler));
   }
-  getMembers():Observable<Member[]> {
-    return this.http.get<Member[]>(this._URL)
-           .pipe(catchError(this.errorHandler))
 
+  //URL for get members
+  private _URLMembers = 'someURL';
+  getMembers():Observable<Member[]> {
+    return this.http.get<Member[]>(this._URLMembers)
+           .pipe(catchError(this.errorHandler));
   }
 
 }
